f75a77beaf1450636803dbae868058a410282eff
gabrieldhofer <leirbagabriel468@gmail.com>
2025-04-28 21:06:04 -0500

    gg

---
+++ data-eng-task-2/in_order.sql
@@ -0,0 +1,10 @@
+CREATE TABLE in_order (
+  id INTEGER,
+  placed_order_id INTEGER,
+  offer_id INTEGER,
+  menu_item_id INTEGER,
+  quantity INTEGER,
+  item_price DECIMAL(12,2),
+  price DECIMAL(12,2),
+  comment VARCHAR(255)
+)
