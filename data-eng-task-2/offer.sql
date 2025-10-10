77e88075a4dde8f0ec077b7525e678cab2f0924a
gabrieldhofer <leirbagabriel468@gmail.com>
2025-10-10 10:52:04 -0500

    gg

--- data-eng-task-2/offer.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE in_order (
-  id INTEGER,
-  date_active_from DATE,
-  time_active_from DATETIME,
-  date_active_to DATETIME,
-  time_active_to DATETIME,
-  offer_price DECIMAL(12,2),
-  PRIMARY KEY(id)
-)
