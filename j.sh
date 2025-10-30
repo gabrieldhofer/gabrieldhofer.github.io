9287c023f1c22d168d9a716c25d1776179b2f2c8
gabrieldhofer <leirbagabriel468@gmail.com>
2025-10-30 09:14:40 -0500

    gg

--- j.sh
+++ j.sh
@@ -1,18 +1,27 @@
 j(){
   # ++-----------------------++
-  #       || author: gabriel hofer ||
+  # || author: gabriel hofer ||
   # ++-----------------------++
-  #https://stackoverflow.com/questions/54161556/how-can-i-show-all-the-branches-in-a-repository
+  # https://stackoverflow.com/questions/54161556/how-can-i-show-all-the-branches-in-a-repository
   clear -x
-  echo -e "-------\e[1;94m--------- GIT BRANCHES -------------\e[0m"
-  git branch 
-  echo -e "-------\e[1;94m+-------- ALIASES ------------------\e[0m"
-  printf "\e[0;35m  %-4s | %-4s \e[0m\n" "dsk" "$HOME/Desktop/"
-  printf "\e[0;35m  %-4s | %-4s \e[0m\n" "ek" "$HOME/Desktop/explore-kaggle/may30"
-  printf "\e[0;35m  %-4s | %-4s \e[0m\n" "io" "$HOME/Desktop/gabrieldhofer.github.io"
-  printf "\e[0;35m  %-4s | %-4s \e[0m\n" "res" "$HOME/Desktop/Resumes"
-  printf "\e[0;35m  %-4s | %-4s \e[0m\n" "adv" "$HOME/Desktop/advent24"
-  echo -e "-------\e[1;94m+-------- FILES --------------------\e[0m"
+  if [ -d .git ]
+  then
+    echo -e "---------------- GIT BRANCHES -------------"
+    CURRENT=$(git branch --show-current);
+    i=1
+    for branch in $(git branch --list | sed 's/\* //g'); do                                         
+      if [[ $branch = $CURRENT ]]
+      then
+        printf "\e[1;32m  %-4s | %-4s \e[0m\n" "b$i" "$branch"
+      else 
+        printf "\e[1;30m  %-4s | %-4s \e[0m\n" "b$i" "$branch"
+      fi 
+      alias "b$i"=" git switch $branch ; j "      
+      i=$((i+1))              
+    done
+  fi 
+  # files first 
+  echo -e "-------+-------- FILES --------------------"
   i=1
   for f in *
   do
@@ -20,31 +29,29 @@ j(){
     then
       if [[ -x $f ]]
       then 
-        if [[ $f == "__sept05_2024__.py" || $f == "__ad_hoc__.py" ]]
+        if [[ $f == "main.sql" ]]
         then
-          #echo -e "\e[1;35m     $i. $f \e[0m"
-          printf "\e[1;35m  %-4s | %-4s \e[0m\n" "$i" "$f"
+          printf "\e[1;35m  %-4s | %-4s \e[0m\n" "f$i" "$f"
         else
-          #echo -e "\e[1;36m     $i. $f \e[0m"
-          printf "\e[1;36m  %-4s | %-4s \e[0m\n" "$i" "$f"
+          printf "\e[1;36m  %-4s | %-4s \e[0m\n" "f$i" "$f"
         fi
       else 
-        #echo -e "\e[1;36m     $i. $f \e[0m"
-        printf "\e[1;36m  %-4s | %-4s \e[0m\n" "$i" "$f"
+        printf "\e[1;36m  %-4s | %-4s \e[0m\n" "f$i" "$f"
       fi 
-      alias $i=" vi '$f' "      
+      alias "f$i"=" vi '$f' "      
       i=$((i+1))              
     fi 
   done
-  echo -e "-------\e[1;94m+-------- DIRS ---------------------\e[0m"
+  # directories second
+  echo -e "-------+-------- DIRS ---------------------"
+  i=1
   for f in * 
   do 
     if [[ -d $f ]]
     then 
-      printf "\e[1;33m  %-4s | %-4s \e[0m\n" "$i" "$f"
-      alias $i=" cd '$f' ; j "      
+      printf "\e[1;33m  %-4s | %-4s \e[0m\n" "d$i" "$f"
+      alias "d$i"=" cd '$f' ; j "      
       i=$((i+1))
     fi 
   done
 }
-
